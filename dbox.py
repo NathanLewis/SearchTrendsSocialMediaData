349976a2924935efb81745e56674b8a1e0e292fa
Nathan Lewis <nathanlewis42@gmail.com>
2025-03-01 00:50:40 +0000

    Now Downloading to local Data directory

--- dbox.py
+++ dbox.py
@@ -5,10 +5,18 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+#from datetime import datetime
 import time
+import os
+
+options = Options()
+prefs = { 'download.prompt_for_download': False,
+        "download.default_directory" : f'{os.path.dirname(os.path.abspath(__file__))}/Data' }
+options.add_experimental_option("prefs", prefs)
 
 # Initialize the WebDriver
-driver = webdriver.Chrome()  # Replace with your preferred browser
+driver = webdriver.Chrome(options=options)  
 
 try:
     # Navigate to your webpage
@@ -17,19 +25,21 @@ try:
     
     # Wait for dropdown to be clickable
     element = WebDriverWait(driver, 10).until(
-        #EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-action='csv']"))  # Replace with your locator
         #EC.element_to_be_clickable((By.CSS_SELECTOR, "[arial-label='Export']"))  # Replace with your locator
+        # XPATH for the Export button
         EC.presence_of_element_located((By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div[5]/div[1]/c-wiz/div/div[1]/div[3]/div[2]/div[2]/div'))  # Replace with your locator
     )
     element.click()
     
     newelement = WebDriverWait(driver, 10).until(
+                                                    # XPATH for the Download CSV dropdown item
         EC.presence_of_element_located((By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div[5]/div[1]/c-wiz/div/div[1]/div[3]/div[2]/div[2]/div/div[2]/div/div/ul/li[1]'))
     )
 
     newelement.click()
     
-    input("Press Enter to exit")
+    #input("Press Enter to exit")
+    time.sleep(2)
     
 finally:
     # Close the browser window
