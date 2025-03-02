842883028a17ef6fd5fa6a6cbfe53c4e15d28363
Nathan Lewis <nathanlewis42@gmail.com>
2025-03-02 13:31:35 +0000

    Replaced the old code which used the pytrends module with new code that uses selenium to drive chrome to download a csv file. Google does not provide an api. The pytrends used an unofficial api and it worked well when it did work. Since then google appears to have broken the assumptions pytrends relied on. A new approach was needed. The new approach is to use selenium to drive chrome to download the trends as a csv file. This *appears* to be more reliable and it gets more data and more detailed data.

--- trends.py
+++ trends.py
@@ -1,18 +1,54 @@
 #!/usr/bin/env python
 
-import pandas as pd
-import re
-from pytrends.request import TrendReq
-from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import random
+import time
+import os
 
+# Sleeping for random amounts of time in hopes of appearing more human
+time.sleep(random.randrange(2, 120, 1))
 
-pytrend = TrendReq()
-for country in ['US', 'GB']: # why does google use GB not UK?
-    trendingtoday = pytrend.today_searches(pn=country)
-    now = datetime.now()
-    filename = f'Data/{country}/today_searches_' + now.strftime("%Y-%m-%dT%H_%M_%S") + '.txt'
-    with open(filename, 'w') as file:
-            for item in trendingtoday:
-                term = re.split(r'=|\&', item)[1]
-                trending_term = term.replace('+',' ')
-                file.write(f'{trending_term}\n')
+options = Options()
+prefs = { 'download.prompt_for_download': False,
+        "download.default_directory" : f'{os.path.dirname(os.path.abspath(__file__))}/Data' }
+options.add_experimental_option("prefs", prefs)
+options.add_argument("--headless")
+options.add_argument("window-size=1920,1080")
+
+# Initialize the WebDriver
+driver = webdriver.Chrome(options=options)  
+
+try:
+    # Navigate to your webpage
+    driver.get("https://trends.google.com/trending?geo=US")
+    time.sleep(random.randrange(3, 4, 1))
+    
+    # Wait for dropdown to be clickable
+    element = WebDriverWait(driver, 100).until(
+        #EC.element_to_be_clickable((By.CSS_SELECTOR, "[arial-label='Export']"))  # Replace with your locator
+        # XPATH for the Export button
+        EC.presence_of_element_located((By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div[5]/div[1]/c-wiz/div/div[1]/div[3]/div[2]/div[2]/div'))  # Replace with your locator
+    )
+    element.click()
+    
+    time.sleep(random.randrange(1, 3, 1))
+
+    newelement = WebDriverWait(driver, 10).until(
+                                                    # XPATH for the Download CSV dropdown item
+        EC.presence_of_element_located((By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div[5]/div[1]/c-wiz/div/div[1]/div[3]/div[2]/div[2]/div/div[2]/div/div/ul/li[1]'))
+    )
+
+    time.sleep(random.randrange(2, 5, 1))
+
+    newelement.click()
+    
+    time.sleep(random.randrange(4, 6, 1))
+    
+finally:
+    # Close the browser window
+    driver.quit()
