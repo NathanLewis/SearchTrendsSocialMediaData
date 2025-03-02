0bc2a23eb6fd092938d58dfb5d01f3ce4c51c856
Nathan Lewis <nathanlewis42@gmail.com>
2025-03-02 11:42:44 +0000

    added headless mode to allow this to run in the cloud and it also seems to fix the problem of not getting data at times

--- dbox.py
+++ dbox.py
@@ -10,12 +10,15 @@ import random
 import time
 import os
 
-#time.sleep(random.randrange(2, 120, 1))
+# Sleeping for random amounts of time in hopes of appearing more human
+time.sleep(random.randrange(2, 120, 1))
 
 options = Options()
 prefs = { 'download.prompt_for_download': False,
         "download.default_directory" : f'{os.path.dirname(os.path.abspath(__file__))}/Data' }
 options.add_experimental_option("prefs", prefs)
+options.add_argument("--headless")
+options.add_argument("window-size=1920,1080")
 
 # Initialize the WebDriver
 driver = webdriver.Chrome(options=options)  
@@ -23,26 +26,28 @@ driver = webdriver.Chrome(options=options)
 try:
     # Navigate to your webpage
     driver.get("https://trends.google.com/trending?geo=US")
-    time.sleep(random.randrange(2, 6, 1))
+    time.sleep(random.randrange(3, 4, 1))
     
     # Wait for dropdown to be clickable
-    element = WebDriverWait(driver, 10).until(
+    element = WebDriverWait(driver, 100).until(
         #EC.element_to_be_clickable((By.CSS_SELECTOR, "[arial-label='Export']"))  # Replace with your locator
         # XPATH for the Export button
         EC.presence_of_element_located((By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div[5]/div[1]/c-wiz/div/div[1]/div[3]/div[2]/div[2]/div'))  # Replace with your locator
     )
     element.click()
     
+    time.sleep(random.randrange(1, 3, 1))
+
     newelement = WebDriverWait(driver, 10).until(
                                                     # XPATH for the Download CSV dropdown item
         EC.presence_of_element_located((By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div[5]/div[1]/c-wiz/div/div[1]/div[3]/div[2]/div[2]/div/div[2]/div/div/ul/li[1]'))
     )
 
-    time.sleep(random.randrange(1, 2, 1))
+    time.sleep(random.randrange(2, 5, 1))
 
     newelement.click()
     
-    time.sleep(random.randrange(2, 6, 1))
+    time.sleep(random.randrange(4, 6, 1))
     
 finally:
     # Close the browser window
