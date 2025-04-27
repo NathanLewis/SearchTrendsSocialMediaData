c64fae028ed047d5bfbf7731254497f64b7e0765
Nathan Lewis <nathanlewis42@gmail.com>
2025-04-28 00:36:38 +0100

    refactored into importable file

--- modcsv.py
+++ modcsv.py
@@ -2,14 +2,6 @@
 import csv
 import sys
 
-contents = {}
-if len(sys.argv) > 1:
-    fname = sys.argv[1]
-    #print(fname)
-    print(fname.replace('original', 'modified'))
-else:
-    print('Usage: ./modcsv.py <csv filename>')
-    exit(-1)
 
 def fix_volume(row: dict[str, str]):
     volume = row['Search volume']
@@ -19,21 +11,34 @@ def fix_volume(row: dict[str, str]):
     row['Search volume'] = volume
     return row
 
-with open(fname) as csvfile:
-    reader = csv.DictReader(csvfile)
-    row1 = next(reader)
-    sheetheader = list(row1.keys())
-    #print(sheetheader)
-    #newsheetheader = sheetheader[0:2] + ['volume +'] + sheetheader[2:]
-    #print(newsheetheader)
-    #print(row1.values())
-    with open(fname.replace('original', 'modified'), 'w') as outputcsv:
-        writer = csv.DictWriter(outputcsv, fieldnames=sheetheader)
-        writer.writeheader()
-        row1 = fix_volume(row1)
-        #print(row1)
-        writer.writerow(row1)
-        for row in reader:
-            row = fix_volume(row)
-            #print(row.values())
-            writer.writerow(row)
+def fix_csvfile(fname: str, inputname: str, outputname: str):
+    with open(fname) as csvfile:
+        reader = csv.DictReader(csvfile)
+        row1 = next(reader)
+        sheetheader = list(row1.keys())
+        #print(sheetheader)
+        #newsheetheader = sheetheader[0:2] + ['volume +'] + sheetheader[2:]
+        #print(newsheetheader)
+        #print(row1.values())
+        with open(fname.replace(inputname, outputname), 'w') as outputcsv:
+            writer = csv.DictWriter(outputcsv, fieldnames=sheetheader)
+            writer.writeheader()
+            row1 = fix_volume(row1)
+            #print(row1)
+            writer.writerow(row1)
+            for row in reader:
+                row = fix_volume(row)
+                #print(row.values())
+                writer.writerow(row)
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) > 1:
+        fname = sys.argv[1]
+        #print(fname)
+        fix_csvfile(fname, 'watchdog', 'processed')
+    else:
+        print('Usage: ./modcsv.py <csv filename>')
+        exit(-1)
+
